3cdcb9b91bff56a906ae4b2f2736985ba25acb42
Olivier-true <olivier.truong@outlook.fr>
2025-12-18 17:46:59 +0100

    commit-2025-12-18 17:46:59

--- p2p_udp_hole_punching.py
+++ p2p_udp_hole_punching.py
@@ -43,7 +43,7 @@ class NATClient:
         while self.running:
             try:
                 print(f"[{self.cid}] Waiting to recv...")
-                data, addr = self.sock.recvfrom(1500)
+                data, addr = self.sock.recvfrom(1472)
                 print("data length recv:", len(data), "connected:", self.connected)
                 if not(self.connected):
                     msg = data.decode(errors="ignore").strip()
@@ -97,8 +97,8 @@ class NATClient:
         if isinstance(data, str):
             data = data.encode()
 
-        for i in range(len(data)//1500 + 1):
-            chunk = data[i*1500:(i+1)*1500]
+        for i in range(len(data)//1472 + 1):
+            chunk = data[i*1472:(i+1)*1472]
             self.sock.sendto(chunk, self.peer)
         
 
